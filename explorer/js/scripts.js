e0c11cea8d504f3ff54534945238769f9750de49
Cristobal E <criesca@gmail.com>
2020-05-20 04:36:59 -0500

    Improvements to time display Consolidation of checks for strings and hyperlinks of blocks and tags

--- explorer/js/scripts.js
+++ explorer/js/scripts.js
@@ -43,6 +43,10 @@ function onEnter(e) {
 
 function timeSince(timevar) {
     var secs = moment(Date.now()).diff(timevar)/1000;
+    var years = Math.floor(secs / (3600*24*365));
+    secs -= years*3600*24*365;
+    var weeks = Math.floor(secs / (3600*24*7));
+    secs -= weeks*3600*24*7;
     var days = Math.floor(secs / (3600*24));
     secs    -= days*3600*24;
     var hrs  = Math.floor(secs / 3600);
@@ -51,10 +55,12 @@ function timeSince(timevar) {
     secs     = Math.floor(secs - mnts*60);
 
     var result="";
-    if(days != 0) { result += days + "d "; }
-    if( hrs != 0) { result +=  hrs + "h "; }
-    if(mnts != 0) { result += mnts + "m "; }
-    if(secs != 0) { result += secs + "s "; }
+    if( years != 0) { result += years + "y "; }
+    if( weeks != 0) { result += weeks + "w "; }
+    if(  days != 0) { result +=  days + "d "; }
+    if(   hrs != 0) { result +=   hrs + "h "; }
+    if(  mnts != 0) { result +=  mnts + "m "; }
+    if(  secs != 0) { result +=  secs + "s "; }
 
     return(result);
 }
@@ -74,12 +80,13 @@ function transverse(obj, result) {
             break;
             case "balance": case "amount": case "fee": 
               processed = (obj[i]/1000000000/1000000000).toLocaleString('en-US', {useGrouping: true, minimumFractionDigits: 4, maximumFractionDigits: 18, style: 'decimal'}) + ' AE'
+              processed = "<span title='" + obj[i].toLocaleString('en-US', {useGrouping: true, maximumFractionDigits: 0, style: 'decimal'}) + " aettos'>" + processed + "</span>"
             break;
             case "block_height": case "height":
               processed = "<a href='/explorer/blocks.html?height=" + obj[i] + "'>" + obj[i] + "</a>"
             break;
             case "recipient_id": case "account_id": case "sender_id": case "miner": case "beneficiary": case "id": case "caller_id":
-              if(obj[i].startsWith("ak_")) {
+              if(typeof(obj[i]) == "string" && obj[i].startsWith("ak_")) {
                 processed = "<a href='/explorer/address.html?address=" + obj[i] + "'>" + obj[i] + "</a>"
                 break;
               }
@@ -88,26 +95,27 @@ function transverse(obj, result) {
                 processed = "<a href='/explorer/transactions.html?from=" + GetURLParameter('height') + "'>" + obj[i] + "</a>"
                 break;
               }
-            case "block_hash": case "prev_hash":
-              if(obj[i].startsWith("mh_")) {
-                processed = "<a href='/explorer/blocks.html?microblock=" + obj[i] + "'>" + obj[i] + "</a>"
-                break;
-              }
             case "payload":
-              if(obj[i].startsWith("ba_")) {
+              if(typeof(obj[i]) == "string" && obj[i].startsWith("ba_")) {
                 r = atob(obj[i].substr(3))
                 if(r.length > 4) {
-                  processed = /* obj[i] + "<br>(Decoded: [" + */ r.substr(0,r.length-4) /* + "])" */
+                  processed = "<span title='" + obj[i] + "'>" + r.substr(0,r.length-4) + "</span> " 
                 }
                 break;
               }
-            case "hash":
-              if(obj[i].startsWith("th_")) {
+            case "prev_hash": case "prev_key_hash": //kh_
+            case "block_hash": case "prev_hash":  //mh_
+            case "hash": //th_ mh_ kh_
+              if(typeof(obj[i]) == "string" && obj[i].startsWith("th_")) {
                 processed = "<a href='/explorer/tx.html?hash=" + obj[i] + "'>" + obj[i] + "</a>"
                 break;
               }
-              if(obj[i].startsWith("mh_")) {
+              if(typeof(obj[i]) == "string" && obj[i].startsWith("mh_")) {
                 processed = "<a href='/explorer/blocks.html?microblock=" + obj[i] + "'>" + obj[i] + "</a>"
+                break;
+              }
+              if(typeof(obj[i]) == "string" && obj[i].startsWith("kh_")) {
+                processed = "<a href='/explorer/blocks.html?hash=" + obj[i] + "'>" + obj[i] + "</a>"
                 break;
               }
             default:
